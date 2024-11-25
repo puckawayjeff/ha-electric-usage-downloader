f030fdc020fa6774ecf2dba2e69ffbc994e889ad
puckawayjeff <jeff@puckaway.net>
2024-11-25 15:00:44 -0600

    Update const.py

--- custom_components/ha_electric_usage_downloader/const.py
+++ custom_components/ha_electric_usage_downloader/const.py
@@ -1,4 +1,4 @@
 from datetime import timedelta
 
-DOMAIN = "ha_electric_usage_downloader"
+DOMAIN = "ha_electric_usage_downloader_acec"
 SCAN_INTERVAL = timedelta(minutes=15)  # Fetch data every 15 minutes
